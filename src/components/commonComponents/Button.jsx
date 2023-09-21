52d59f342b4f074c09f3265f1909b38a7de2cb1d
Furkan <FSDeryol@gmail.com>
2023-09-21 23:38:30 +0300

    react slideshow image library added for homepage sections

--- src/components/commonComponents/Button.jsx
+++ src/components/commonComponents/Button.jsx
@@ -1,16 +1,33 @@
 import React from "react"
 import styles from "../../styles/componentStyles/commonComponentStyles/button.module.css"
 
-export default function Button({ isNav, isWide, content }) {
-  return isNav ? (
-    <button
-      className={`${styles.button} ${styles.button_nav} ${
-        isWide ? styles.button_wide : null
-      }`}
-    >
-      <a>{content}</a>
-    </button>
-  ) : (
-    <button>{content}</button>
-  )
+export default function Button({ type, isWide, content }) {
+  let button
+
+  switch (type) {
+    case "navigation":
+      button = (
+        <button
+          className={`${styles.button} ${styles.button_nav} ${
+            isWide ? styles.button_wide : null
+          }`}
+        >
+          <a>{content}</a>
+        </button>
+      )
+      break
+    case "homepage-content":
+      button = (
+        <button
+          className={`${styles.button} ${styles.button_homepage_content}`}
+        >
+          {content}
+        </button>
+      )
+
+    default:
+      break
+  }
+
+  return button
 }
--- src/components/homepageComponents/Navigation.jsx
+++ src/components/homepageComponents/Navigation.jsx
@@ -7,29 +7,34 @@ export default function Navigation() {
     {
       content: "Projects",
       isWide: false,
+      type: "navigation",
     },
     {
       content: "Services",
       isWide: false,
+      type: "navigation",
     },
     {
       content: "About us",
       isWide: false,
+      type: "navigation",
     },
     {
       content: "Contact us",
       isWide: false,
+      type: "navigation",
     },
     {
       content: "Start a Project",
       isWide: true,
+      type: "navigation",
     },
   ]
 
   return (
     <nav className={styles.navigation_container}>
       {navigation.map((button, index) => {
-        return <Button key={index} isNav={true} {...button} />
+        return <Button key={index} {...button} />
       })}
     </nav>
   )
